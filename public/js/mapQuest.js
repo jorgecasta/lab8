dd4d95292bee5a8a55209bf64dc050687dd2a1c3
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-02-25 10:21:10 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = "KHOgqRsuygAy7qyjh6tgPGEh5Bv8tLy8";
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map("map", {
+    center: [32.8784549,-117.2371714],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 12,
+  });
+
+  L.marker([32.8784549,-117.2371714]).addTo(map);
 }
